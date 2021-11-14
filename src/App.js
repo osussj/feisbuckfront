ed263822a934591e645d5ef046e7d8a5c2bde022
osu <joanosuna9@gmail.com>
2021-11-14 17:59:37 +0100

    Added RegisterForm component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import "./App.css";
-import Login from "./components/Login/Login";
+import RegisterForm from "./components/RegisterForm/RegisterForm";
 
 function App() {
   return (
     <div className="App">
-      <Login />
+      <RegisterForm />
     </div>
   );
 }
---
+++ src/components/RegisterForm/RegisterForm.js
@@ -0,0 +1,88 @@
+import { useState } from "react";
+import useUsers from "../../hooks/useUsers";
+
+const RegisterForm = () => {
+  const initialData = {
+    name: "",
+    username: "",
+    password: "",
+    age: "",
+  };
+  const { createUser } = useUsers();
+  const [userRegisterData, setUserRegisterData] = useState(initialData);
+
+  const changeData = (event) => {
+    setUserRegisterData({
+      ...userRegisterData,
+      [event.target.id]: event.target.value,
+    });
+  };
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    const newUser = {
+      name: userRegisterData.name,
+      username: userRegisterData.username,
+      password: userRegisterData.password,
+      age: userRegisterData.age,
+    };
+    createUser(newUser);
+  };
+
+  return (
+    <div className="row align-items-center justify-content-center vh-100">
+      <div className="col-md-5">
+        <form
+          className="login-form"
+          onSubmit={onSubmit}
+          autoComplete="off"
+          noValidate
+        >
+          <div className="mb-3">
+            <input
+              type="text"
+              id="name"
+              onChange={changeData}
+              className="form-control"
+              placeholder="Enter your name"
+            />
+          </div>
+          <div className="mb-3">
+            <input
+              type="text"
+              id="username"
+              onChange={changeData}
+              className="form-control"
+              placeholder="Enter your username"
+            />
+          </div>
+          <div className="mb-3">
+            <input
+              type="password"
+              onChange={changeData}
+              id="password"
+              className="form-control"
+              placeholder="Enter your password"
+            />
+          </div>
+          <div className="mb-3">
+            <input
+              type="number"
+              id="age"
+              onChange={changeData}
+              min="0"
+              max="120"
+              className="form-control"
+              placeholder="Enter your age"
+            />
+          </div>
+          <div className="text-center pt-3 pb-3">
+            <button className="btn btn-success btn-lg mt-3">Register</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default RegisterForm;

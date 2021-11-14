073e585263efb619da881be65919897850101f2c
osu <joanosuna9@gmail.com>
2021-11-14 21:02:14 +0100

    Added better user experience on Login

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -24,43 +24,47 @@ const Login = () => {
   };
 
   return (
-    <div className="row align-items-center justify-content-center vh-100">
-      <div className="col-md-5">
-        <form
-          className="login-form"
-          onSubmit={onSubmit}
-          autoComplete="off"
-          noValidate
-        >
-          <div className="mb-3">
-            <input
-              type="text"
-              id="username"
-              className="form-control"
-              placeholder="Email address or phone number"
-              onChange={changeData}
-            />
-          </div>
-          <div className="mb-3">
-            <input
-              type="password"
-              id="password"
-              className="form-control"
-              placeholder="Password"
-              onChange={changeData}
-            />
-          </div>
-          <button className="btn btn-custom btn-lg btn-block mt-3">
-            Login
-          </button>
-          <div className="text-center pt-3 pb-3">
-            <Link to="/register">
-              <button type="button" className="btn btn-success btn-lg mt-3">
-                Create New Account
+    <div className="container">
+      <div className="row align-items-center justify-content-center vh-100">
+        <div className="col-md-5">
+          <form
+            className="login-form"
+            onSubmit={onSubmit}
+            autoComplete="off"
+            noValidate
+          >
+            <div className="mb-3">
+              <input
+                type="text"
+                id="username"
+                className="form-control"
+                placeholder="Username"
+                onChange={changeData}
+              />
+            </div>
+            <div className="mb-3">
+              <input
+                type="password"
+                id="password"
+                className="form-control"
+                placeholder="Password"
+                onChange={changeData}
+              />
+            </div>
+            <div className="text-center pt-3 pb-3">
+              <button className="btn btn-custom btn-lg btn-block mt-3">
+                Login
               </button>
-            </Link>
-          </div>
-        </form>
+            </div>
+            <div className="text-center pt-3 pb-3">
+              <Link to="/register">
+                <button type="button" className="btn btn-success btn-lg mt-3">
+                  Create New Account
+                </button>
+              </Link>
+            </div>
+          </form>
+        </div>
       </div>
     </div>
   );

fdcd31e2eda868a624f56f03835df43b37fb43fd
osu <joanosuna9@gmail.com>
2021-11-14 19:27:04 +0100

    Added registerPage

---
+++ src/pages/registerPage/registerPage.js
@@ -0,0 +1,12 @@
+import RegisterForm from "../../components/RegisterForm/RegisterForm";
+
+const RegisterPage = () => {
+  return (
+    <>
+      <h1 className="main-title">Register Page</h1>
+      <RegisterForm />
+    </>
+  );
+};
+
+export default RegisterPage;

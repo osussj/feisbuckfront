5d4e811beec7d5c9031f360ae3ced9e68174c3f8
osu <joanosuna9@gmail.com>
2021-11-14 16:22:06 +0100

    Added loginUserThunk

---
+++ src/redux/thunks/userThunks.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+import jwtDecode from "jwt-decode";
+import { loginUserAction } from "../actions/actionCreators";
+
+const urlApi = process.env.REACT_APP_URL_API;
+export const loginUserThunk = (user) => async (dispatch) => {
+  const response = await axios.post(`${urlApi}users/login`, user);
+  console.log(response);
+  if (response.status === 200) {
+    const token = response.data.token;
+    const user = jwtDecode(token);
+    dispatch(loginUserAction(user));
+    localStorage.setItem("user", JSON.stringify({ token: token }));
+  }
+};

b648c68464fb280041148e12172bb4295fa3528f
osu <joanosuna9@gmail.com>
2021-11-15 19:45:11 +0100

    Added getUsers to custom hook

--- src/hooks/useUsers.js
+++ src/hooks/useUsers.js
@@ -2,10 +2,14 @@ import jwtDecode from "jwt-decode";
 import { useCallback } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { isLoggedAction } from "../redux/actions/actionCreators";
-import { createUserThunk, loginUserThunk } from "../redux/thunks/userThunks";
+import {
+  createUserThunk,
+  loadUsersThunk,
+  loginUserThunk,
+} from "../redux/thunks/userThunks";
 
 const useUsers = () => {
-  const { user } = useSelector((user) => user);
+  const { user, users } = useSelector(({ user, users }) => ({ user, users }));
   const dispatch = useDispatch();
 
   const loginUser = (user) => {
@@ -23,11 +27,18 @@ const useUsers = () => {
       dispatch(isLoggedAction(userInfo));
     }
   }, [dispatch]);
+
+  const loadUsers = useCallback(() => {
+    dispatch(loadUsersThunk());
+  }, [dispatch]);
+
   return {
     user,
+    users,
     loginUser,
     createUser,
     isLogged,
+    loadUsers,
   };
 };
 

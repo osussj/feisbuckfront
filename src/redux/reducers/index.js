bc0fa375f0181c39c2f136aa9e044b08913eb0be
osu <joanosuna9@gmail.com>
2021-11-15 19:45:35 +0100

    Added getUsers to usersReducers

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import userReducer from "./userReducer";
+import usersReducer from "./usersReducer";
 
 const rootReducer = combineReducers({
   user: userReducer,
+  users: usersReducer,
 });
 
 export default rootReducer;
